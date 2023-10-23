03cf711c6b6a2a42deee8f91e1b70b8e833215e0
LEIF LUNDBERG <leif.lundberg@ibm.com>
2023-10-23 07:37:05 +0200

    Addded logging, verified docker compose working

--- main.go
+++ main.go
@@ -3,48 +3,53 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func uploadFile(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("File Upload Endpoint Hit")
+	log.Println("Request received from: " + r.Host)
 	r.ParseMultipartForm(1000 << 20)
 	file, handler, err := r.FormFile("myFile")
 	if err != nil {
-		fmt.Println("Error Retrieving the File")
-		fmt.Println(err)
+		log.Println(err)
+		fmt.Fprintf(w, "Error retrieving the file")
 		return
 	}
 	defer file.Close()
 	filename := handler.Filename
-	fmt.Printf("Uploaded File: %+v\n", filename)
-	fmt.Printf("File Size: %+v\n", handler.Size)
-	fmt.Printf("MIME Header: %+v\n", handler.Header)
+	log.Println("Filename: " + filename)
+	log.Println("Size in bytes: " + strconv.FormatInt(handler.Size, 10))
 	extension := filename[strings.LastIndex(filename, ".")+1:]
-	fmt.Println("Extension: ." + extension)
+	log.Println("Extension: ." + extension)
 	tempFile, err := os.CreateTemp("uploads", "upload-*."+extension)
 	if err != nil {
-		fmt.Println("Permission denied here? inside CreateTemp")
-		fmt.Println(err)
+		log.Println(err)
+		fmt.Fprintf(w, "Error creating temporary file")
 	}
 	defer tempFile.Close()
 	fileBytes, err := io.ReadAll(file)
 	if err != nil {
-		fmt.Println("Permission denied here? inside ReadAll")
-		fmt.Println(err)
+		log.Println(err)
+		fmt.Fprintf(w, "Error reading received file")
 	}
 	tempFile.Write(fileBytes)
-	fmt.Fprintf(w, "Successfully Uploaded File\n")
+	log.Println("File successfully uploaded")
+	fmt.Fprintf(w, "Successfully uploaded file: "+filename+"\n")
 }
 
-func setupRoutes() {
+func router(port string) {
 	http.HandleFunc("/upload", uploadFile)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(port, nil)
 }
 
 func main() {
-	fmt.Println("Media upload endpoint is listening...")
-	setupRoutes()
+	log.Println("Starting app...")
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	port := ":8080"
+	log.Println("Media upload endpoint is listening on " + port)
+	router(port)
 }
